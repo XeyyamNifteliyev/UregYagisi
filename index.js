4719840e3cc8e0c1da9ba51fecc8f40963f14619
XeyyamNifteliyev <132565870+XeyyamNifteliyev@users.noreply.github.com>
2023-12-30 00:02:03 +0400

    repeat

--- index.js
+++ index.js
@@ -34,19 +34,19 @@
 // setInterval(urekYarat,300)
 
 
-function urekDuzelt(){
- let urek =  document.createElement('div')
- 
- urek.classList.add("urek")
- urek.innerHTML = "&#10084 "
- urek.style.left = Math.random()*100 + "vw"
- urek.style.animationDuration = Math.random() * 100 + 5 + 3 + "s"
-urek.style.color = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
- document.body.appendChild(urek)
+function urekDuzelt() {
+   let urek = document.createElement('div')
 
- setTimeout(()=>{
-    urek.remove()
- },10000)
+   urek.classList.add("urek")
+   urek.innerHTML = "&#10084 "
+   urek.style.left = Math.random() * 100 + "vw"
+   urek.style.animationDuration = Math.random() * 100 + 5 + 3 + "s"
+   urek.style.color = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+   document.body.appendChild(urek)
+
+   setTimeout(() => {
+      urek.remove()
+   }, 10000)
 }
 
-setInterval(urekDuzelt,10)
+setInterval(urekDuzelt, 10)
